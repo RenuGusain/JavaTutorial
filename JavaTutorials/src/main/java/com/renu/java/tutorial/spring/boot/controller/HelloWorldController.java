caee87fa6dc54c6717ce19f280b29d98903eac58
renu <renu.gusain05@gmail.com>
2025-03-18 23:13:40 +0530

    dd

--- JavaTutorials/src/main/java/com/renu/java/tutorial/spring/boot/controller/HelloWorldController.java
+++ JavaTutorials/src/main/java/com/renu/java/tutorial/spring/boot/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @GetMapping
+    @GetMapping("/renu")
     public ResponseEntity<String>hello()
     {
         return  ResponseEntity.status(HttpStatus.OK).body("Hello Wrold! I am Renu");
